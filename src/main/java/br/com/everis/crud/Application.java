d03afe45c528db1d126f235057c06b990884a3f2
Lucas Oliveira Galdino <lolivega@everis.com>
2020-06-09 14:39:49 -0300

    First commit

---
+++ src/main/java/br/com/everis/crud/Application.java
@@ -0,0 +1,26 @@
+package br.com.everis.crud;
+
+import br.com.everis.crud.domain.Post;
+import br.com.everis.crud.domain.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private PostRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        for (int i = 0; i < 5; i++) {
+            repository.save(new Post("My post number #" + (i+1)));
+        }
+    }
+}
---
+++ src/main/java/br/com/everis/crud/domain/PostRepository.java
@@ -0,0 +1,6 @@
+package br.com.everis.crud.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}
